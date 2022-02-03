eed40aca0de2d9ce516542333e157a5635c8a6ba
MukeshVenkat <mukiv0802@gmail.com>
2022-02-03 23:50:19 +0530

    Components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproductComponent } from './product/addproduct/addproduct.component';
+import { EditProductComponent } from './product/edit-product/edit-product.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { AddProductComponent } from './product/add-product/add-product.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddproductComponent,
+    EditProductComponent,
+    ProductListComponent,
+    AddProductComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
